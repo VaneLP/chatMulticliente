e258c3f488e7d86722fe985b81a02487cd226ac2
Vane <123445952+VaneLP@users.noreply.github.com>
2024-01-08 09:59:24 +0100

    comentarios

--- src/main/java/com/example/chatmulticliente/Aplicacion.java
+++ src/main/java/com/example/chatmulticliente/Aplicacion.java
@@ -12,15 +12,21 @@ public class Aplicacion extends Application {
     public void start(Stage stage) throws IOException {
         //para agregar el css
         Application.setUserAgentStylesheet(String.valueOf(Aplicacion.class.getResource("dracula.css")));
-
+        //leemos el FXML
         FXMLLoader fxmlLoader = new FXMLLoader(Aplicacion.class.getResource("vista-chat.fxml"));
+        //redimensionamos la ventana
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        //agregamos un titulo a la ventana
         stage.setTitle("Chat Multicliente");
+        //asignamos la ventana
         stage.setScene(scene);
+        //mostramos la ventana
         stage.show();
     }
 
+    // ---- MAIN ----
     public static void main(String[] args) {
+        //Lanzamos la la aplicacion
         launch();
     }
 }
